dd7d2a38b597ea05b88a1364544437bac26b178d
Johannes Andersson <hello@thejoltjoker.com>
2024-10-21 14:23:56 +0200

    feat: working connection to api

---
+++ lightroom-llama.lrplugin/Info.lua
@@ -0,0 +1,18 @@
+return {
+    VERSION = {
+        major = 1,
+        minor = 0,
+        revision = 0
+    },
+    LrPluginName = "Lightroom Llama",
+    LrPluginDescription = "Description of your Lightroom plugin",
+    LrToolkitIdentifier = "com.thejoltjoker.lightroom.llama",
+    LrPluginInfoUrl = "https://github.com/thejoltjoker/lightroom-llama",
+    LrPluginInfoUrlProvider = "http://www.thejoltjoker.com",
+    LrSdkVersion = 10.0,
+    LrSdkMinimumVersion = 5.0,
+    LrLibraryMenuItems = {{
+        title = "Lightroom Llama...",
+        file = "LrLlama.lua"
+    }}
+}
